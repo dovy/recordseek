0ce15f4c6cf4a7a6f25ebdc3f38779a52ac293c3
Dovy <dovy@reduxframework.com>
2015-06-20 10:58:55 -0600

    Instantiate fast click.

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -8,6 +8,14 @@ RecordSeek.helpers = {
 
 };
 
+if ( 'addEventListener' in document ) {
+    document.addEventListener(
+        'DOMContentLoaded', function() {
+            FastClick.attach( document.body );
+        }, false
+    );
+}
+
 
 /**
  * @ngdoc overview
